9b7694feb4313ee3ac71001607ba4a28bd371498
Santiago Lacki <santiagolacki@gmail.com>
2025-08-13 20:19:14 -0300

    Se pueden ver los datos que llegan en la solicitud de agregar un producto

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -8,8 +8,17 @@ export const leerProductos = (req, res) => {
 }
 
 // Agregar funcion para crear productos
-export const crearProducto = (req,res) => {
-  
+export const crearProducto = async (req,res) => {
+  try {
+    // 1- Recibir el objeto que tengo que agregar a la BD
+    console.log(req.body)
+    // 2- Validar los datos
+    // 3- Agregar el producto a la BD
+    // 4- Devolver un mensaje de confirmacion
+  }
+  catch (error) {
+    console.error(error)
+  }
 }
 
 // Aagregar funcion para editar productos
