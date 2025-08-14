af6030f690051f1047695c497b79ab48ec13ca8d
Santiago Lacki <santiagolacki@gmail.com>
2025-08-13 21:41:58 -0300

    Se agrego la funcionalidad para dar de alta un producto

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,3 +1,5 @@
+import Producto from "../models/producto.js";
+
 export const test = (req, res) => {
   res.status(200);
   res.send('Primera prueba desde el backend');
@@ -11,13 +13,17 @@ export const leerProductos = (req, res) => {
 export const crearProducto = async (req,res) => {
   try {
     // 1- Recibir el objeto que tengo que agregar a la BD
-    console.log(req.body)
+    // console.log(req.body)
     // 2- Validar los datos
     // 3- Agregar el producto a la BD
+    const nuevoProducto = new Producto(req.body);
+    await nuevoProducto.save();
     // 4- Devolver un mensaje de confirmacion
+    res.status(201).json({mensaje: 'Producto creado con exito'});
   }
   catch (error) {
     console.error(error)
+    res.status(500).json({mensaje: 'Error al crear producto'});
   }
 }
 
